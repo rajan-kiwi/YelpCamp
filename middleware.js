f2ecc23d9ece85483cb1bfebd418d9eaca13bc3e
rajan-kiwi <86221596+rajan-kiwi@users.noreply.github.com>
2023-04-12 15:42:00 +0530

    changes to test

--- middleware.js
+++ middleware.js
@@ -3,14 +3,14 @@ const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
-module.exports.isLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.session.returnTo = req.originalUrl
-        req.flash('error', 'You must be signed in first!');
-        return res.redirect('/login');
-    }
-    next();
-}
+// module.exports.isLoggedIn = (req, res, next) => {
+//     if (!req.isAuthenticated()) {
+//         req.session.returnTo = req.originalUrl
+//         req.flash('error', 'You must be signed in first!');
+//         return res.redirect('/login');
+//     }
+//     next();
+// }
 
 module.exports.validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
